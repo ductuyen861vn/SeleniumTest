643f9be566e2b8cc6381a981d59d21e4c1d0d000
Tuyen <ductuyen861vn@gmail.com>
2017-05-25 13:50:10 +0700

    Push sourcecode JAVA2

--- src/test/java/ExecuseTest/TestUploadFileTest.java
+++ src/test/java/ExecuseTest/TestUploadFileTest.java
@@ -1,8 +1,10 @@
 package ExecuseTest;
 
 import ObjectPage.GoogleLogin;
+import ObjectPage.GoogleMail;
 import ObjectPage.Homepage;
 import ObjectPage.LoginPage;
+import org.junit.After;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -11,6 +13,7 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.*;
@@ -21,6 +24,9 @@ import static org.testng.Assert.*;
 public class TestUploadFileTest {
     WebDriver driver;
     GoogleLogin googleLogin;
+    GoogleMail googleMail;
+
+
 
     @BeforeTest
     public void setup(){
@@ -29,20 +35,49 @@ public class TestUploadFileTest {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://gmail.com");
         googleLogin = new GoogleLogin(driver);
+        googleMail = new GoogleMail(driver);
+
 
 
     }
 
     @Test(priority = 0)
-    public void login(){
+    public void login() throws InterruptedException {
+
         googleLogin.inputEmail();
         googleLogin.clickNext();
-        WebDriverWait wait = new WebDriverWait(driver,10);
+        WebDriverWait wait = new WebDriverWait(driver,10);;
         wait.until(ExpectedConditions.visibilityOfElementLocated(GoogleLogin.password));
         googleLogin.inputPass();
         googleLogin.clickNext();
 
+
     }
+
+
+    @Test(priority = 1)
+    public void openUploadFile() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver,10);;
+        wait.until(ExpectedConditions.visibilityOfElementLocated(GoogleMail.googleAccount));
+        GoogleMail.mainHandle = driver.getWindowHandle();
+        System.out.println(driver.getWindowHandle());
+        googleMail.clickOnGoogleAcc();
+        googleMail.clickOnProfile();
+
+        Set<String> winHandles = driver.getWindowHandles();
+        for (String handle:winHandles){
+            if(GoogleMail.mainHandle.equalsIgnoreCase(handle)){
+                WebDriver popup = driver.switchTo().window(handle);
+            }
+        }
+
+        System.out.println(winHandles);
+
+
+
+    }
+
+
 
 
 
